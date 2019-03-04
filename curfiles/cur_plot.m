14bb7e1b5ea8eedbfdca99692c9517be025adad0
Albert J. G <el17jg@leeds.ac.uk>
2019-03-04 14:23:08 +0000

    Delay when ki = 0.3, from 75-125sec.

--- curfiles/cur_plot.m
+++ curfiles/cur_plot.m
@@ -1,5 +1,22 @@
+clear all
 
-A = importdata('temp_display_0.15-0.03.cur');
-x = A(:,1)
-y = A(:,4)
-plot(x,y)
+A1 = importdata('temp_display_0.45-0.25.cur')
+x1 = A1(:,1)
+y1 = A1(:,4)
+plot(x1,y1)
+hold on
+
+A2 = importdata('temp_display_0.45-0.25_delay-50-100.cur')
+x2 = A2(:,1)
+y2 = A2(:,4)
+plot(x2,y2)
+
+
+ 
+legend('No delay', 'delay from 50 to 100sec')
+ 
+
+title('Machine g2')
+xlabel('t(s)') % x-axis label
+ylabel('Omega(pµ)') % y-axis label
+grid on
