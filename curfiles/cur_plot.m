e697705a565668d5c9a78321cbfe3719c8a72715
Albert J. G <el17jg@leeds.ac.uk>
2019-03-08 00:02:41 +0000

    Update MATLAB: export to excel

--- curfiles/cur_plot.m
+++ curfiles/cur_plot.m
@@ -2,26 +2,43 @@
 
 s1 = 'temp_display_';
 s2 = 's';
-s3 = '.cur';
+s3 = '.cur'
 
 figure();
 hold on
-for kp = 0.10:0.10:0.70
-    for ki = 0.10:0.10:0.70
-        for delay = 0.00:0.10:1.0
+T = [];
+KP = [];
+KI = [];
+DELAY = [];
+SETTLINGTIME = [];
+
+for kp = 0.3
+    for ki = 0.3
+        for delay = 0.00:0.10:1.00
             if delay == 0.00
                 delay = 0.01
             end
             
             s = [s1 num2str(kp,'%.2f') '-' num2str(ki,'%.2f') '-' num2str(delay,'%.2f') s2 s3]
             a = importdata(s)
-            x0 = a(:,1)
-            y0 = a(:,4)
-            txt = ['kp = ', num2str(kp), ', ki = ', num2str(ki), ', delay = ',num2str(delay)];
-            plot(x0, y0, 'DisplayName',txt)
+            t = a(:,1)
+            f = a(:,4)
+            txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ', delay = ',num2str(delay,'%.3f')];
+            plot(t, f, 'DisplayName',txt)
+            info = stepinfo(f)
+            settlingTime = info.SettlingTime
+            KP = [KP; kp];
+            KI = [KI; ki];
+            DELAY = [DELAY; delay];
+            SETTLINGTIME = [SETTLINGTIME; settlingTime];
+            
+            
         end
     end
 end
+
+T = table(KP, KI, DELAY, SETTLINGTIME)
+writetable(T,'somefile.xlsx');
 hold off
 
 legend show
@@ -32,3 +49,4 @@ title(theTitle)
 xlabel('t(s)')
 ylabel('Omega(pµ)')
 grid on
+
