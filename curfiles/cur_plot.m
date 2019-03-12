ee0bcfba267119d12f4546c4e3ee5bae1a87b2f2
Albert J. G <el17jg@leeds.ac.uk>
2019-03-12 17:49:51 +0000

    Update

--- curfiles/cur_plot.m
+++ curfiles/cur_plot.m
@@ -1,18 +1,15 @@
 % start : step point : end
-
 s1 = 'temp_display_';
 s2 = 's';
 s3 = '.cur';
-
 figure();
 hold on
 KP = [];
 KI = [];
 DELAY = [];
 SETTLINGTIME = [];
-
 for kp = 0.10
-    for ki = 0.30
+    for ki = 0.10:0.50:20.10
         for delay = 0.01
             s = [s1 num2str(kp,'%.2f') '-' num2str(ki,'%.2f') '-' num2str(delay,'%.2f') s2 s3];
             a = importdata(s);
@@ -29,16 +26,12 @@ for kp = 0.10
         end
     end
 end
-
 T = table(KP, KI, DELAY, SETTLINGTIME);
-writetable(T,'somefile.xlsx');
+writetable(T,'rank.xlsx');
 hold off
-
 legend show
-
 theTitle = 'Machine g2';
 title(theTitle)
-
 xlabel('t(s)')
 ylabel('Omega(pµ)')
 grid on
