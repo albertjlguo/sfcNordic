e6d129c84a283cfe1e57a4d8d92e5dea419a8977
Albert J. G <el17jg@leeds.ac.uk>
2019-03-13 17:51:36 +0000

    Update

--- curfiles/cur_plot.m
+++ curfiles/cur_plot.m
@@ -1,4 +1,5 @@
 % start : step point : end
+
 s1 = 'temp_display_';
 s2 = 's';
 s3 = '.cur';
@@ -8,31 +9,33 @@ KP = [];
 KI = [];
 DELAY = [];
 SETTLINGTIME = [];
-for kp = 0.10
-    for ki = 0.10:0.50:20.10
+for kp = 1.60
+    for ki = 4.10:0.50:20.10
         for delay = 0.01
             s = [s1 num2str(kp,'%.2f') '-' num2str(ki,'%.2f') '-' num2str(delay,'%.2f') s2 s3];
             a = importdata(s);
             t = a(:,1);
             f = a(:,4);
+            info = stepinfo(f);
             txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ', delay = ',num2str(delay,'%.3f')];
             plot(t, f, 'DisplayName',txt)
-            info = stepinfo(f);
             settlingTime = info.SettlingTime;
             KP = [KP; kp];
             KI = [KI; ki];
             DELAY = [DELAY; delay];
             SETTLINGTIME = [SETTLINGTIME; settlingTime];
+            
         end
     end
 end
 T = table(KP, KI, DELAY, SETTLINGTIME);
+xlsx_dic = '\Users\el17jg\Desktop\GitHub\Nordic-test-system\curfiles\rank.xlsx'; % save to a dictionary
 writetable(T,'rank.xlsx');
 hold off
+
 legend show
 theTitle = 'Machine g2';
 title(theTitle)
 xlabel('t(s)')
 ylabel('Omega(pµ)')
 grid on
-
