f60db048445137041071c04b4145bb62587b1bff
Albert J. G <el17jg@leeds.ac.uk>
2019-03-26 17:06:11 +0000

    Nordic log 2.2

--- MATLAB_PLOT/cur_plot.m
+++ MATLAB_PLOT/cur_plot.m
@@ -12,6 +12,8 @@ prefix_Win = 'D:/OneDrive - University of Leeds/';
 Nordic_Data = 'Nordic/Data/';
 excel_name = 'rank.xlsx';
 
+folder_name = '2.2';
+
 figure();
 hold on
 KP = [];
@@ -19,54 +21,9 @@ KI = [];
 DELAY = [];
 SETTLINGTIME = [];
 
-for kp = 0.1:10:540.1
-    %{
-    if kp>=0.1 && kp<=49.6  % kp: 0.1-49.6
-    coef = 0.6341;
-    end
-
-    if kp>=50.1 && kp<=99.6  % kp: 50.1-99.6
-        coef = 0.5709;
-    end
-
-    if kp>=100.1 && kp<=149.6  % kp: 100.1-149.6
-        coef = 0.5355;
-    end
-
-    if kp>=150.1 && kp<=199.6  % kp: 150.1-199.6
-        coef = 0.4837;
-    end
-
-    if kp>=200.1 && kp<=249.6  % kp: 200.1-249.6
-        coef = 0.4153;
-    end
-
-    if kp>=250.1 && kp<=299.6  % kp: 250.1-299.6
-        coef = 0.3583;
-    end
-
-    if kp>=300.1 && kp<=349.6  % kp: 300.1-349.6
-        coef = 0.2919;
-    end
-
-    if kp>=350.1 && kp<=399.6  % kp: 350.1-399.6
-        coef = 0.2288;
-    end
-
-    if kp>=400.1 && kp<=449.6  % kp: 400.1-449.6
-        coef = 0.1602;
-    end
-
-    if kp>=450.1 && kp<=499.6  % kp: 450.1-499.6
-        coef = 0.1602;
-    end
-
-    if kp>=500.1 && kp<=539.6  % kp: 500.1-539.6
-        coef = 0.0432;
-    end
-    %}
-    for ki = 0.1:10:(coef*kp)
-        for delay = 0.01:0.10:0.22
+for kp = 250.1
+    for ki = 0.1:10:250.1
+        for delay = 0.01
             s = [s1 num2str(kp,'%.2f') '-' num2str(ki,'%.2f') '-' num2str(delay,'%.2f') s2 s3];
             a = importdata(s);
             t = a(:,1);
@@ -89,8 +46,6 @@ for kp = 0.1:10:540.1
     end
 end
 T = table(KP, KI, DELAY, SETTLINGTIME);
-
-folder_name = '2.1';
 xlsx_dic = [prefix_Win Nordic_Data folder_name '/' excel_name];  % save to a dictionary
 writetable(T,xlsx_dic);
 hold off
