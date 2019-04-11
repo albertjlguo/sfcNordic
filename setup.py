dc7e05dc8ecc5e88ca59333a79305392c0dd0165
Albert J. G <el17jg@leeds.ac.uk>
2019-04-11 17:45:02 +0100

    add example

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if __name__ == "__main__":
+    setup(name="ehtim",
+          version = "0.0.3",
+          author = "Jialeng Guo",
+          author_email = "el17jg@leeds.ac.uk",
+          description = ("Python code to load, simulate"),
+        # license = "GPLv3",
+          keywords = "Centralized control algorithms for Nordic Smart Grid operation.",
+          url = "https://github.com/realgjl/Nordic-Test-System",
+          packages = ["examples"],
+          long_description=read('README.rst'),
+          install_requires=["PyRAMSES",
+                            "numpy",
+                            "os",
+                            "shutil",
+                            "sys"]
+    )
