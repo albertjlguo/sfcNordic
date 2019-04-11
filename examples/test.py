3325de4275919e3b66a1a9f41b3e15aa684c1cc3
Albert J. G <el17jg@leeds.ac.uk>
2019-04-11 14:19:19 +0100

    delete python scripts

--- examples/test.py
+++ examples/test.py
@@ -9,7 +9,7 @@ from agc import agc
 
 if __name__ == '__main__':
     
-    prepared_folder_address = 'D:/OneDrive - University of Leeds/Nordic/cur_g12' 
+    prepared_folder_address = 'C:/Users/el17jg/Desktop/GitHub/Nordic-test-system/testFolder' 
     breaker = 'g12' 
     
     monitor = ['g2']
@@ -20,11 +20,11 @@ if __name__ == '__main__':
     '''
     tuning delay & kp & ki:
     '''
-    for td in np.arange(0.06, 0.22, 0.01):  # td: 0.06~0.21 sec, step: 0.01
+    for td in np.arange(0.01, 0.02, 0.01):  # td: 0.01~0.1 sec, step: 0.01
         td = "{0:.2f}".format(round(td,2))
-        for kp in np.arange(0.1, 325.2, 5.0):  # kp: 0.1~325.1, step: 5.0
+        for kp in np.arange(0.1, 50.2, 5.0):  # kp: 0.1~50.1, step: 5.0
             kp = "{0:.2f}".format(round(float(kp),2))
-            for ki in np.arange(0.1, 25.2, 5.0):  # ki: 0.1-25.1, step: 5.0
+            for ki in np.arange(0.1, 0.2, 5.0):  # ki: 0.1-0.1, step: 5.0
                 ki = "{0:.2f}".format(round(float(ki),2))
 
                 print("kp = " + str(kp))
