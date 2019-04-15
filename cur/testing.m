7fe2d424ac432476eff1ec36f4060dfe4c33f94c
Albert J. G <el17jg@leeds.ac.uk>
2019-04-15 18:39:40 +0100

    update for g8

--- cur/testing.m
+++ cur/testing.m
@@ -15,9 +15,9 @@ KP = [];
 KI = [];
 DELAY = [];
 SETTLINGTIME = [];
-for kp = 0.1:5:90.1
-    for ki = 0.1:0.1:0.5
-        for delay = 0.01
+for kp = 45.1
+    for ki = 0.10:2.90:3.0
+        for delay = 0.20
             s = ['temp_display_g8_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's', '.cur'];
             a = importdata(s);
             t = a(:,1);
@@ -27,6 +27,16 @@ for kp = 0.1:5:90.1
         end
     end
 end
+hold on
+kp = 30.00;
+ki = 5.00;
+delay = 0.20;
+s = ['temp_display_g8_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's', '.cur'];
+a = importdata(s);
+t = a(:,1);
+f = a(:,4);
+txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ', delay = ',num2str(delay,'%.2f')];
+plot(t, f, 'DisplayName',txt,'LineWidth',1)
 hold off
 
 legend show
@@ -34,7 +44,7 @@ theTitle = ['Machine g2 (Nordic: 0.998 ~ 1.002): AGC starting from ' num2str(sta
 title(theTitle)
 xlabel('t(s)')
 ylabel('Omega(pµ)')
-xlim([0 369]);
+xlim([0 650]);
 ylim([0.99 1.01]);
 grid on
 
