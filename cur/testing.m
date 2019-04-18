f64a808217357534f2a80ef0e171ebce135ad736
Albert J. G <el17jg@leeds.ac.uk>
2019-04-18 16:27:16 +0100

    before zip td

--- cur/testing.m
+++ cur/testing.m
@@ -8,44 +8,41 @@
 % start : step point : end
 startingTime = 25;
 endingTime = 240;
+nordic_limit = 0.2;  % Nordic: 1±0.2%
+gb_limit = 0.4;  % GB: 1±0.4%
+
+% input folder (store cur files)
+curFolder = 'D:/OneDrive - University of Leeds/GB/cur';
 
 figure();
 hold on
-KP = [];
-KI = [];
-DELAY = [];
-SETTLINGTIME = [];
-for kp = 45.1
-    for ki = 0.10:2.90:3.0
-        for delay = 0.20
-            s = ['temp_display_g8_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's', '.cur'];
+
+breaker = 'g4';
+for delay = 0.01  % delay: 0.01s
+    for kp = 10.1  % kp: 10.1
+        for ki = 0.1  % ki: 0.1
+            s = ['temp_display_', breaker, '_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's.cur'];
+            s
             a = importdata(s);
             t = a(:,1);
             f = a(:,4);
-            txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ', delay = ',num2str(delay,'%.2f')];
-            plot(t, f, 'DisplayName',txt,'LineWidth',1)
+            % set steady-state value (y_final) to nominal value & SettlingTimeThreshold to 4%:
+            info = stepinfo(f,t,1.0,'SettlingTimeThreshold',0.04);
+            settlingTime = info.SettlingTime;
+            txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ...
+                ', Delay = ', num2str(delay,'%.2f'), ' sec, Settling Time = ', num2str(settlingTime,'%.4f'), ' sec'];
+            plot(t, f, 'DisplayName',txt)
+            
         end
     end
 end
-hold on
-kp = 30.00;
-ki = 5.00;
-delay = 0.20;
-s = ['temp_display_g8_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's', '.cur'];
-a = importdata(s);
-t = a(:,1);
-f = a(:,4);
-txt = ['kp = ', num2str(kp,'%.2f'), ', ki = ', num2str(ki,'%.2f'), ', delay = ',num2str(delay,'%.2f')];
-plot(t, f, 'DisplayName',txt,'LineWidth',1)
 hold off
 
 legend show
-theTitle = ['Machine g2 (Nordic: 0.998 ~ 1.002): AGC starting from ' num2str(startingTime) ' sec, system ending at ' num2str(endingTime) ' sec'];
+theTitle = ['Machine g4 (Nordic: 0.998 ~ 1.002): AGC starting from ', num2str(startingTime), ' sec, system ending at ', num2str(endingTime), ' sec'];
 title(theTitle)
 xlabel('t(s)')
 ylabel('Omega(pµ)')
-xlim([0 650]);
+xlim([0 999]);
 ylim([0.99 1.01]);
 grid on
-
-
