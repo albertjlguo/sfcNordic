fc154b2036f5b311c426ab3c3119e1b5b019248b
Albert J. G <el17jg@leeds.ac.uk>
2019-04-25 00:55:00 +0100

    Create impact_of_gens.m

---
+++ analysis/6.1/impact_of_gens.m
@@ -0,0 +1,44 @@
+% testing.m
+%
+% This ia MATLAB program use for plotting only.
+% Put a value or a range of kp, ki, delay into the following program.
+% p.s.: file name, e.g.: temp_display_0.00-0.00-0.01s.cur
+%       the file should be in the same direction with this testing program.
+
+% start : step point : end
+%startingTime = ;
+%endingTime = 900;
+
+figure();
+hold on
+KP = [];
+KI = [];
+DELAY = [];
+SETTLINGTIME = [];
+breaker_list = {'g1', 'g2', 'g3', 'g4', 'g5', 'g8', 'g9', 'g10', 'g11', 'g12', 'g13', 'g17', 'g18', 'g19', 'g20'};
+
+hold on
+for i = 1:length(breaker_list)
+    breaker = breaker_list{i};
+
+    kp = 0;
+    ki = 0;
+    delay = 0.01;
+    s = ['/Users/realgjl/Desktop/GitHub/sfcNordic/analysis/4.1/',...
+        'temp_display_', breaker, '_', num2str(kp,'%.2f'), '-', num2str(ki,'%.2f'), '-', num2str(delay,'%.2f'), 's', '.cur'];
+    a = importdata(s);
+    t = a(:,1);
+    f = a(:,4);
+    txt = breaker;
+    plot(t, f, 'DisplayName',txt,'LineWidth',1)
+end
+
+hold off
+    legend show
+    theTitle = ['without Secondary Frequency Control'];
+    title(theTitle)
+    xlabel('t(s)')
+    ylabel('Omega(pµ)')
+    xlim([0 950]);
+    ylim([0.99 1.01]);
+    grid on
